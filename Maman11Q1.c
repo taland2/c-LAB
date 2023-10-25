81c79754abfc131dc29b61351b17af7e212b0ff1
taland2 <taland222@gmail.com>
2023-10-25 11:38:08 +0300

    first commit

---
+++ Maman11Q1.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void) {
+  int a = 10;
+  int b = 20;
+
+  // הוסף את הערכים של a ו-b
+  int sum = a + b;
+
+  // הדפס את התוצאה
+  printf("התוצאה היא: %d\n", sum);
+
+  return 0;
+}
