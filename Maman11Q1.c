14756f38a682da1ab6426095b665dfad8607f1db
taland2 <taland222@gmail.com>
2023-10-25 15:36:05 +0300

    check:

--- Maman11Q1.c
+++ Maman11Q1.c
@@ -1,14 +1,38 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main(void) {
-  int a = 10;
-  int b = 20;
+  /*input dollar- shekel ratio*/ 
+  float dollar_rate;
+  scanf("%f", &dollar_rate);
 
-  // הוסף את הערכים של a ו-b
-  int sum = a + b;
+  float dollar_amounts[100];
+  int num_dollar_amounts = 0;
+  while (1) {
+    float dollar_amount;
+    scanf("%f", &dollar_amount);
+    if (dollar_amount == 0) {
+      break;
+    }
+    dollar_amounts[num_dollar_amounts++] = dollar_amount;
+  }
+    float shekel_amount;
+  /*for every dollar amount*/
+  for (int i = 0; i < num_dollar_amounts; i++) {
+    /*compute shekel amount*/ 
+    float shekel_amount = dollar_amounts[i] * dollar_rate;
 
-  // הדפס את התוצאה
-  printf("התוצאה היא: %d\n", sum);
+    printf("%10.2f | %10.2f\n", dollar_amounts[i], shekel_amount);
+  }
+
+  /*print sums*/ 
+  float total_dollars = 0;
+  float total_shekels = 0;
+  for (int i = 0; i < num_dollar_amounts; i++) {
+    total_dollars += dollar_amounts[i];
+    total_shekels += shekel_amounts[i];
+  }
+  printf("DOLLAR: %.2f\nSHEKEL: %.2f\n", total_dollars, total_shekels);
 
   return 0;
 }
